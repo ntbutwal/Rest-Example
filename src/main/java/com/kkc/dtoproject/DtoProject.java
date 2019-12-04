8b245ef2a11010cdde9fe9971641fdea696d3265
ntbutwal <ntbutwal@gmail.com>
2019-12-04 14:23:24 -0500

    Rest Example

---
+++ src/main/java/com/kkc/dtoproject/DtoProject.java
@@ -0,0 +1,41 @@
+package com.kkc.dtoproject;
+
+public class DtoProject {
+	private int id;
+	private String projName;
+	private String location;
+	private String email;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getProjName() {
+		return projName;
+	}
+
+	public void setProjName(String projName) {
+		this.projName = projName;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
